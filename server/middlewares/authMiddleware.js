05777036748a70f8cb0516d827ed6c096a11d48e
oksanapetrosky <oksanapetrosky@gmail.com>
2025-04-01 15:19:14 -0400

    Add authMiddleware protectRoute

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -14,3 +14,21 @@ export const protectEducator = async (req, res, next) => {
     res.json({ success: false, message: error.message });
   }
 };
+// Middleware: Protect any logged-in user route
+export const protectRoute = async (req, res, next) => {
+  try {
+    const userId = req.auth.userId;
+    const response = await clerkClient.users.getUser(userId);
+
+    req.user = {
+      _id: userId,
+      email: response.emailAddresses[0].emailAddress,
+      fullName: response.firstName + " " + response.lastName,
+      imageUrl: response.imageUrl,
+    };
+
+    next();
+  } catch (error) {
+    res.status(401).json({ success: false, message: "Unauthorized access" });
+  }
+};
