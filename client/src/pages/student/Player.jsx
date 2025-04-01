c3231dfca30b816d034a28eb6e7ef60c344e8f17
oksanapetrosky <oksanapetrosky@gmail.com>
2025-04-01 10:43:00 -0400

    Updated code for AddCourse and Player files

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -3,21 +3,38 @@ import { AppContext } from "../../context/AppContext";
 import { useParams } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
-import YouTube from 'react-youtube';
+import YouTube from "react-youtube";
 import Footer from "../../components/student/Footer";
 import Rating from "../../components/student/Rating";
+import axios from "axios";
+import { toast } from "react-toastify";
+import Loading from "../../components/student/Loading";
 
 const Player = () => {
-  const { enrolledCourses, calculateChapterTime } = useContext(AppContext);
+  const {
+    enrolledCourses,
+    calculateChapterTime,
+    backendUrl,
+    getToken,
+    userData,
+    fetchUserEnrolledCourses,
+  } = useContext(AppContext);
   const { courseId } = useParams();
   const [courseData, setCourseData] = useState(null);
   const [openSection, setOpenSection] = useState({});
   const [playerData, setPlayerData] = useState(null);
+  const [progressData, setProgressData] = useState(null);
+  const [initialRating, setInitialRating] = useState(0);
 
   const getCourseData = () => {
     enrolledCourses.map((course) => {
       if (course._id === courseId) {
         setCourseData(course);
+        course.courseRatings.map((item) => {
+          if (item.userId === userData._id) {
+            setInitialRating(item.rating);
+          }
+        });
       }
     });
   };
@@ -27,10 +44,68 @@ const Player = () => {
   };
 
   useEffect(() => {
-    getCourseData();
+    if (enrolledCourses.length > 0) {
+      getCourseData();
+    }
   }, [enrolledCourses]);
 
-  return (
+  const markLectureAsCompleted = async (lectureId) => {
+    try {
+      const token = await getToken();
+      const { data } = await axios.post(
+        backendUrl + "/api/user/update-course-progress",
+        { courseId, lectureId },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getCourseProgress()
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const getCourseProgress = async () => {
+    try {
+      const token = await getToken();
+      const { data } = await axios.post(
+        backendUrl + '/api/user/get-course-progress', {courseId}, { headers: { Authorization: `Bearer ${token}` }}
+      );
+
+    if(data.success){
+      setProgressData(data.progressData)
+    } else{
+      toast.error(data.message)
+    }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  };
+
+  const handleRate = async (rating) => {
+    try {
+      const token = await getToken()
+      const {data} = await axios.post(backendUrl + '/api/user/add-rating', {courseId, rating}, {headers: { Authorization: `Bearer ${token}` }})
+      if (data.success) {
+        toast.success(data.message)
+        fetchUserEnrolledCourses()
+      } else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(()=> {
+   getCourseProgress()
+  },[])
+
+  return courseData ? (
     <>
       <div
         className="p-4 sm:p-10 flex flex-col-reverse md:grid 
@@ -83,7 +158,7 @@ const Player = () => {
                         <li key={i} className="flex items-start gap-2 py-1">
                           <img
                             src={
-                              false ? assets.blue_tick_icon : assets.play_icon
+                              progressData && progressData.lectureCompleted.includes(lecture.lectureId) ? assets.blue_tick_icon : assets.play_icon
                             }
                             alt="play_icon"
                             className="w-4 h-4 mt-1"
@@ -123,32 +198,44 @@ const Player = () => {
                 </div>
               ))}
           </div>
-          
-        <div className="flex items-center gap-2 py-3 mt-10">
-          <h1 className="text-xl font-bold">Rate this Course:</h1>
-          <Rating initialRating={0}/>
-        </div>
-        </div>
 
+          <div className="flex items-center gap-2 py-3 mt-10">
+            <h1 className="text-xl font-bold">Rate this Course:</h1>
+            <Rating initialRating={initialRating} onRate={handleRate}/>
+
+          </div>
+        </div>
 
         {/* Right column */}
         <div className="md:mt-10">
           {playerData ? (
             <div>
-             <YouTube videoId={playerData.lectureUrl.split('/').pop()} iframeClassName="w-full aspect-video"/>
-            <div className="flex justify-between items-center mt-1">
-              <p>{playerData.chapter}.{playerData.lecture} {playerData.lectureTitle}</p>
-              <button className="text-blue-600">{false ? 'Completed' : 'Mark Complete'}</button>
+              <YouTube
+                videoId={playerData.lectureUrl.split("/").pop()}
+                iframeClassName="w-full aspect-video"
+              />
+              <div className="flex justify-between items-center mt-1">
+                <p>
+                  {playerData.chapter}.{playerData.lecture}{" "}
+                  {playerData.lectureTitle}
+                </p>
+                <button onClick={()=> markLectureAsCompleted(playerData.lectureId)} className="text-blue-600">
+                  {progressData && progressData.lectureCompleted.includes(playerData.lectureId) ? "Completed" : "Mark Complete"}
+                </button>
+              </div>
             </div>
-            </div>
-          )
-          :
-          <img src={courseData ? courseData.courseThumbnail : ''} alt="thumbnail" />}
+          ) : (
+            <img
+              src={courseData ? courseData.courseThumbnail : ""}
+              alt="thumbnail"
+            />
+          )}
         </div>
       </div>
       <Footer />
     </>
-  );
+  ) : <Loading />
+
 };
 
 export default Player;
