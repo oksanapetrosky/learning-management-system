bc85ee3e9c61ad007db90b0da16af0847c2599c9
oksanapetrosky <oksanapetrosky@gmail.com>
2025-04-01 12:18:14 -0400

    User.js wasn't impoorted

--- server/configs/multer.js
+++ server/configs/multer.js
@@ -6,25 +6,4 @@ const upload = multer({ storage })
 
 export default upload
 
-// import multer from 'multer';
-
-// // Configure storage (Multer will store files in memory, we send to Cloudinary directly)
-// const storage = multer.memoryStorage();
-
-// // Validate file type and size
-// const fileFilter = (req, file, cb) => {
-//   if (!file.mimetype.startsWith("image/")) {
-//     return cb(new Error("Only image files are allowed!"), false);
-//   }
-//   cb(null, true);
-// };
-
-// // Set upload limits (5MB max size)
-// const upload = multer({ 
-//   storage, 
-//   fileFilter, 
-//   limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
-// });
-
-// export default upload;
 
